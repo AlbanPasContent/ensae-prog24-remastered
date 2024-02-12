a0ec9888689485ea75965fe544ddf9de5f240975
AlbanNelias <alban.nelias@ensae.fr>
2024-02-12 13:01:02 +0100

    format to PEP 8

--- swap_puzzle/graph.py
+++ swap_puzzle/graph.py
@@ -2,14 +2,16 @@
 This is the graph module. It contains a minimalistic Graph class.
 """
 
+
 class Graph:
     """
-    A class representing undirected graphs as adjacency lists. 
+    A class representing undirected graphs as adjacency lists.
 
-    Attributes: 
+    Attributes:
     -----------
     nodes: NodeType
-        A list of nodes. Nodes can be of any immutable type, e.g., integer, float, or string.
+        A list of nodes. Nodes can be of any immutable type, e.g., integer,
+        float, or string.
         We will usually use a list of integers 1, ..., n.
     graph: dict
         A dictionnary that contains the adjacency list of each node in the form
@@ -17,50 +19,54 @@ class Graph:
     nb_nodes: int
         The number of nodes.
     nb_edges: int
-        The number of edges. 
+        The number of edges.
     edges: list[tuple[NodeType, NodeType]]
         The list of all edges
     """
 
     def __init__(self, nodes=[]):
         """
-        Initializes the graph with a set of nodes, and no edges. 
+        Initializes the graph with a set of nodes, and no edges.
 
-        Parameters: 
+        Parameters:
         -----------
         nodes: list, optional
             A list of nodes. Default is empty.
         """
-        self.nodes = nodes 
+        self.nodes = nodes
         self.graph = dict([(n, []) for n in nodes])
         self.nb_nodes = len(nodes)
         self.nb_edges = 0
         self.edges = []
-        
+
     def __str__(self):
         """
         Prints the graph as a list of neighbors for each node (one per line)
         """
         if not self.graph:
-            output = "The graph is empty"            
+            output = "The graph is empty"
         else:
-            output = f"The graph has {self.nb_nodes} nodes and {self.nb_edges} edges.\n"
+            output = (f"The graph has {self.nb_nodes} nodes "
+                      f"and {self.nb_edges} edges.\n")
             for source, destination in self.graph.items():
                 output += f"{source}-->{destination}\n"
         return output
 
-    def __repr__(self): 
+    def __repr__(self):
         """
         Returns a representation of the graph with number of nodes and edges.
         """
-        return f"<graph.Graph: nb_nodes={self.nb_nodes}, nb_edges={self.nb_edges}>"
+        return (f"<graph.Graph: nb_nodes={self.nb_nodes}, "
+                f"nb_edges={self.nb_edges}>")
 
     def add_edge(self, node1, node2):
         """
-        Adds an edge to the graph. Graphs are not oriented, hence an edge is added to the adjacency list of both end nodes. 
-        When adding an edge between two nodes, if one of the ones does not exist it is added to the list of nodes.
+        Adds an edge to the graph. Graphs are not oriented, hence an edge is
+        added to the adjacency list of both end nodes.
+        When adding an edge between two nodes, if one of the ones does not
+        exist it is added to the list of nodes.
 
-        Parameters: 
+        Parameters:
         -----------
         node1: NodeType
             First end (node) of the edge
@@ -81,41 +87,44 @@ class Graph:
         self.nb_edges += 1
         self.edges.append((node1, node2))
 
-    def bfs(self, src, dst): 
+    def bfs(self, src, dst):
         """
-        Finds a shortest path from src to dst by BFS.  
+        Finds a shortest path from src to dst by BFS.
 
-        Parameters: 
+        Parameters:
         -----------
         src: NodeType
             The source node.
         dst: NodeType
             The destination node.
 
-        Output: 
+        Output:
         -------
         path: list[NodeType] | None
-            The shortest path from src to dst. Returns None if dst is not reachable from src
-        """ 
-        # TODO: implement this function (and remove the line "raise NotImplementedError").
+            The shortest path from src to dst. Returns None if dst is not
+            reachable from src
+        """
+        # TODO: implement this function.
+        # NOTE: remove the line "raise NotImplementedError".
         raise NotImplementedError
 
     @classmethod
     def graph_from_file(cls, file_name):
         """
-        Reads a text file and returns the graph as an object of the Graph class.
+        Reads a text file and returns the graph as an object of the Graph
+        class.
 
-        The file should have the following format: 
+        The file should have the following format:
             The first line of the file is 'n m'
             The next m lines have 'node1 node2'
         The nodes (node1, node2) should be named 1..n
 
-        Parameters: 
+        Parameters:
         -----------
         file_name: str
             The name of the file
 
-        Outputs: 
+        Outputs:
         -----------
         graph: Graph
             An object of the class Graph with the graph from file_name.
@@ -127,8 +136,7 @@ class Graph:
                 edge = list(map(int, file.readline().split()))
                 if len(edge) == 2:
                     node1, node2 = edge
-                    graph.add_edge(node1, node2) # will add dist=1 by default
+                    graph.add_edge(node1, node2)  # will add dist=1 by default
                 else:
                     raise Exception("Format incorrect")
         return graph
-
--- swap_puzzle/grid.py
+++ swap_puzzle/grid.py
@@ -2,52 +2,54 @@
 This is the grid module. It contains the Grid class and its associated methods.
 """
 
-import random
 
 class Grid():
     """
-    A class representing the grid from the swap puzzle. It supports rectangular grids. 
+    A class representing the grid from the swap puzzle. It supports
+    rectangular grids.
 
-    Attributes: 
+    Attributes:
     -----------
     m: int
         Number of lines in the grid
     n: int
         Number of columns in the grid
     state: list[list[int]]
-        The state of the grid, a list of list such that state[i][j] is the number in the cell (i, j), i.e., in the i-th line and j-th column. 
+        The state of the grid, a list of list such that state[i][j] is the
+        number in the cell (i, j), i.e., in the i-th line and j-th column.
         Note: lines are numbered 0..m and columns are numbered 0..n.
     """
-    
-    def __init__(self, m, n, initial_state = []):
+
+    def __init__(self, m, n, initial_state=[]):
         """
         Initializes the grid.
 
-        Parameters: 
+        Parameters:
         -----------
         m: int
             Number of lines in the grid
         n: int
             Number of columns in the grid
         initial_state: list[list[int]]
-            The intiail state of the grid. Default is empty (then the grid is created sorted).
+            The intiail state of the grid. Default is empty (then the grid is
+            created sorted).
         """
         self.m = m
         self.n = n
         if not initial_state:
-            initial_state = [list(range(i*n+1, (i+1)*n+1)) for i in range(m)]            
+            initial_state = [list(range(i*n+1, (i+1)*n+1)) for i in range(m)]
         self.state = initial_state
 
-    def __str__(self): 
+    def __str__(self):
         """
         Prints the state of the grid as text.
         """
-        output = f"The grid is in the following state:\n"
-        for i in range(self.m): 
+        output = "The grid is in the following state:\n"
+        for i in range(self.m):
             output += f"{self.state[i]}\n"
         return output
 
-    def __repr__(self): 
+    def __repr__(self):
         """
         Returns a representation of the grid with number of rows and columns.
         """
@@ -55,49 +57,59 @@ class Grid():
 
     def is_sorted(self):
         """
-        Checks is the current state of the grid is sorte and returns the answer as a boolean.
+        Checks is the current state of the grid is sorte and returns the
+        answer as a boolean.
         """
-        # TODO: implement this function (and remove the line "raise NotImplementedError").
+        # TODO: implement this function
+        # NOTE : remove the line "raise NotImplementedError".
         raise NotImplementedError
 
     def swap(self, cell1, cell2):
         """
-        Implements the swap operation between two cells. Raises an exception if the swap is not allowed.
+        Implements the swap operation between two cells. Raises an exception
+        if the swap is not allowed.
 
-        Parameters: 
+        Parameters:
         -----------
         cell1, cell2: tuple[int]
-            The two cells to swap. They must be in the format (i, j) where i is the line and j the column number of the cell. 
+            The two cells to swap. They must be in the format (i, j) where i
+            is the line and j the column number of the cell.
         """
-        # TODO: implement this function (and remove the line "raise NotImplementedError").
+        # TODO: implement this function
+        # NOTE : remove the line "raise NotImplementedError".
         raise NotImplementedError
 
     def swap_seq(self, cell_pair_list):
         """
-        Executes a sequence of swaps. 
+        Executes a sequence of swaps.
 
-        Parameters: 
+        Parameters:
         -----------
         cell_pair_list: list[tuple[tuple[int]]]
-            List of swaps, each swap being a tuple of two cells (each cell being a tuple of integers). 
-            So the format should be [((i1, j1), (i2, j2)), ((i1', j1'), (i2', j2')), ...].
+            List of swaps, each swap being a tuple of two cells (each cell
+            being a tuple of integers).
+            So the format should be :
+            [((i1, j1), (i2, j2)), ((i1', j1'), (i2', j2')), ...].
         """
-        # TODO: implement this function (and remove the line "raise NotImplementedError").
+        # TODO: implement this function
+        # NOTE : remove the line "raise NotImplementedError".
         raise NotImplementedError
 
     @classmethod
-    def grid_from_file(cls, file_name): 
+    def grid_from_file(cls, file_name):
         """
-        Creates a grid object from class Grid, initialized with the information from the file file_name.
-        
-        Parameters: 
+        Creates a grid object from class Grid, initialized with the
+        information from the file file_name.
+
+        Parameters:
         -----------
         file_name: str
-            Name of the file to load. The file must be of the format: 
-            - first line contains "m n" 
-            - next m lines contain n integers that represent the state of the corresponding cell
+            Name of the file to load. The file must be of the format:
+            - first line contains "m n"
+            - next m lines contain n integers that represent the state of the
+            corresponding cell
 
-        Output: 
+        Output:
         -------
         grid: Grid
             The grid
@@ -107,10 +119,8 @@ class Grid():
             initial_state = [[] for i_line in range(m)]
             for i_line in range(m):
                 line_state = list(map(int, file.readline().split()))
-                if len(line_state) != n: 
+                if len(line_state) != n:
                     raise Exception("Format incorrect")
                 initial_state[i_line] = line_state
             grid = Grid(m, n, initial_state)
         return grid
-
-
